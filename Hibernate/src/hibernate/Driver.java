8c2c411a4e3e165e72f5050e8aa7cdff2ca41acf
Kasun Gamage <kasun.gamage@webstasy.one>
2017-09-22 22:19:30 +0530

    hibernate

---
+++ Hibernate/src/hibernate/Driver.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate;
+
+import java.util.List;
+import model.pojo.Actor;
+import model.util.NewHibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+/**
+ *
+ * @author Kasun
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+    
+}
